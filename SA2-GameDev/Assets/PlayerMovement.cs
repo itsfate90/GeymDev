8bc0597f8467fae32eefe24f7ee635f7d18c7649
itsfate90 <aaron_joshua_macasaet@dlsl.edu.ph>
2022-06-03 13:56:45 +0800

    Added envrionemt, chacter,charcter movement, collsion and idle animation

---
+++ SA2-GameDev/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    float horizontalMove = 0f;
+    public float runSpeed = 200;
+    bool jump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove=Input.GetAxisRaw("Horizontal")*runSpeed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove*Time.fixedDeltaTime,false,jump);
+        jump=false;
+    }
+}
