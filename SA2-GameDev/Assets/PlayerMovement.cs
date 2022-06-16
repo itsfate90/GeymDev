ad6d771a9c2a33bc7ced001d99905abb017d11c7
itsfate90 <aaron_joshua_macasaet@dlsl.edu.ph>
2022-06-16 15:24:38 +0800

    added jumping animation

--- SA2-GameDev/Assets/PlayerMovement.cs
+++ SA2-GameDev/Assets/PlayerMovement.cs
@@ -9,28 +9,38 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     public float runSpeed = 200;
     bool jump = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalMove=Input.GetAxisRaw("Horizontal")*runSpeed;
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
 
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
+
+
+
     }
+    public void Onlanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
     void FixedUpdate()
     {
-        controller.Move(horizontalMove*Time.fixedDeltaTime,false,jump);
-        jump=false;
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
