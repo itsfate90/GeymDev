dbf4d703187469d74b9eb7373b7fda29ef4e54d8
itsfate90 <aaron_joshua_macasaet@dlsl.edu.ph>
2022-09-20 16:35:30 +0800

    addded collectibles sytem and scoring UI

--- SA2-GameDev/Assets/Scripts/CoinCollectible.cs
+++ SA2-GameDev/Assets/Scripts/CoinCollectible.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoinCollectible : MonoBehaviour
 {
+    private int Coin = 0;
+    [SerializeField] private Text coinText;
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.CompareTag("Coin")){
             Destroy(collision.gameObject);
+            Coin++;
+            coinText.text = "Coins:" + Coin;
         }
     }
 }
