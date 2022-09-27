7c26f3cfdc6e8428af9ad4da8d9fdfaf1f8eb10c
duwpo <duwpolikesrice@gmail.com>
2022-09-27 08:28:08 +0800

    PauseMenu

---
+++ SA2-GameDev/Assets/Scripts/Pause.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public static bool isPaused = false;
+    [SerializeField]  GameObject pauseMenu;
+ 
+
+  
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+            
+        }
+        
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+
+    void PauseGame()
+    {
+       pauseMenu.SetActive(true);
+       Time.timeScale = 0f;
+       isPaused = true;
+
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+        
+    }
+}
