2652a239a6a6cca01e3e8a5c0cb334af77fefdca
itsfate90 <aaron_joshua_macasaet@dlsl.edu.ph>
2022-10-19 15:27:45 +0800

    tested parallax bg

---
+++ SA2-GameDev/Assets/Scripts/ParallaxMethod2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxMethod2 : MonoBehaviour
+{
+    Material mat;
+    float distance;
+
+    [Range(0f,0.5f)]
+    public float speed=0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       mat=GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance += Time.deltaTime*speed;
+        
+    }
+}
