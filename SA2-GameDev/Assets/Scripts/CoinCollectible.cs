040b358a510e58d823ed4f98e132e88a4cc4be1c
itsfate90 <aaron_joshua_macasaet@dlsl.edu.ph>
2022-10-25 21:18:31 +0800

    fixed double coin count

--- SA2-GameDev/Assets/Scripts/CoinCollectible.cs
+++ SA2-GameDev/Assets/Scripts/CoinCollectible.cs
@@ -5,15 +5,19 @@ using UnityEngine.UI;
 
 public class CoinCollectible : MonoBehaviour
 {
-    private int Coin = 0;
+    private int coins = 0;
     [SerializeField] private Text coinText;
     [SerializeField] private AudioSource collectionSoundEffect;
+
     private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.gameObject.CompareTag("Coin")){
+
+        if(collision.gameObject.CompareTag("Coin"))
+        {
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
-            Coin++;
-            coinText.text = "Coins:" + Coin;
+            coins++;
+            coinText.text="Coins:" + coins;
+           
         }
     }
 }
