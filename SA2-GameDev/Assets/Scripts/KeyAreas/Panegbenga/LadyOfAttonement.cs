82c30efdcc306e55f0ed79033ff74e0614028589
duwpo <duwpolikesrice@gmail.com>
2022-10-26 22:28:19 +0800

    rescale some of the GO, Fixed the alignment of texts

---
+++ SA2-GameDev/Assets/Scripts/KeyAreas/Panegbenga/LadyOfAttonement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class LadyOfAttonement : MonoBehaviour
+{
+    public bool isPlayerAlreadyEntered;
+    [SerializeField]  GameObject welcomeSign;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(!isPlayerAlreadyEntered)
+        {
+            if (col.CompareTag("Player"))
+            {
+                isPlayerAlreadyEntered = true;
+                welcomeSign.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            welcomeSign.SetActive(false);
+        }
+    }
+}
