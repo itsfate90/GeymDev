d0a5cbb2b8042d68b08df05d371c703c4ef2348a
duwpo <duwpolikesrice@gmail.com>
2022-10-31 13:29:50 +0800

    _coin >= 1 && _paper  ==3  / also added paper panel

--- SA2-GameDev/Assets/Scripts/CoinCollectible.cs
+++ SA2-GameDev/Assets/Scripts/CoinCollectible.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CoinCollectible : MonoBehaviour
 {
-    private int coins = 0;
+    private int _coins;
+    private int _paper;
     [SerializeField] private Text coinText;
     [SerializeField] private AudioSource collectionSoundEffect;
      [SerializeField] private GameObject portal;
@@ -15,12 +14,18 @@ public class CoinCollectible : MonoBehaviour
         {
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
-            coins++;
-                if(coins==1){
-                Instantiate(portal,new Vector3(1048,-2,0),Quaternion.identity);
-                }
-            coinText.text="Coins:" + coins;
-           
+            _coins++;
         }
+
+        else if (collision.gameObject.CompareTag("Pages"))
+        {
+            Destroy(collision.gameObject);
+            _paper++;
+        }
+        if(_coins >= 1 && _paper == 3){
+            Instantiate(portal,new Vector3(1048,-2,0),Quaternion.identity);
+        }
+        coinText.text="Coins:" + _coins;
+
     }
 }
---
+++ SA2-GameDev/Assets/Scripts/KeyAreas/AtiAtihan/PaperMechanic.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+
+public class PaperMechanic : MonoBehaviour
+{
+    [SerializeField] GameObject dialoguePanel;
+    [SerializeField] GameObject continuePanel;
+    [SerializeField] Image speakerImage;
+    [SerializeField] Sprite speakerSprite;
+    //[SerializeField] TextMeshProUGUI npcName;
+    public TextMeshProUGUI textComponent;
+    
+    public string[] lines;
+    public float textSpeed;
+    public bool isPlayerClose;
+    public bool alreadyCollected;
+    public bool isDone;
+    
+
+    private int _index;
+
+    private void Start()
+    {
+        textComponent.text= String.Empty;
+        alreadyCollected = false;
+        isDone = false;
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        isPlayerClose = col.CompareTag("Player");
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerClose = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerClose && !alreadyCollected)
+        {
+            dialoguePanel.SetActive(true);
+            alreadyCollected = true;
+            StartDialogue();
+            Time.timeScale = 0f;
+        }
+        
+        if (Input.GetMouseButtonDown(0) && isDone)
+        {
+            
+            if (textComponent.text == lines[_index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+            }
+        }
+    }
+
+    void StartDialogue()
+    {
+        speakerImage.sprite = speakerSprite;
+        _index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[_index].ToCharArray())
+        {
+            isDone = false;
+            continuePanel.SetActive(false);
+            textComponent.text += c;
+            yield return new WaitForSecondsRealtime(textSpeed);
+            isDone = true;
+            continuePanel.SetActive(true);
+
+        }
+    }
+
+    void NextLine()
+    {
+        if (_index < lines.Length - 1)
+        {
+            _index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            dialoguePanel.SetActive(false);
+            continuePanel.SetActive(false);
+            Time.timeScale = 1f;
+            textComponent.text= String.Empty;
+        }
+    }
+}
