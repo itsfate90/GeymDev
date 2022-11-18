042f126e1d2c85ae43cb42a7a9a344aef97e37ef
duwpo <duwpolikesrice@gmail.com>
2022-11-18 13:27:22 +0800

    remodify keyarea codes

---
+++ SA2-GameDev/Assets/Scripts/KeyAreas/AtiAtihan/KeyArea.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class KeyArea : MonoBehaviour
+{
+   [SerializeField] private GameObject welcomePanel;
+   public TextMeshProUGUI textComponent;
+   private int _index;
+   public string[] lines;
+   public float textSpeed;
+   
+
+   private void Start()
+   {
+      welcomePanel.SetActive(false);
+   }
+
+   public void OnTriggerEnter2D(Collider2D col)
+   {
+      if (col.CompareTag("Player"))
+      {
+         textComponent.text = String.Empty;
+         StartWelcome();
+         welcomePanel.SetActive(true);
+      }
+   }
+
+   public void OnTriggerExit2D(Collider2D other)
+   {
+      if (other.CompareTag("Player"))
+      {
+         StopAllCoroutines();
+         welcomePanel.SetActive(false);
+      }
+   }
+
+   private void StartWelcome()
+   {
+      _index = 0;
+      StartCoroutine(TypeLine());
+   }
+
+   IEnumerator TypeLine()
+   {
+      foreach (char c in lines[_index].ToCharArray())
+      {
+         textComponent.text += c;
+         yield return new WaitForSecondsRealtime(textSpeed);
+      }
+   }
+}
