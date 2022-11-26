bd422a18b7e8b6b5f4e085bf52da46a43e3aaa35
itsfate90 <aaron_joshua_macasaet@dlsl.edu.ph>
2022-11-26 22:52:42 +0800

    changed sprite and added paper and coin ui to arae 3

---
+++ SA2-GameDev/Assets/Scripts/Area3_Collectible.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Area3_Collectible : MonoBehaviour
+{
+    private int _coins;
+    private int _paper;
+    [SerializeField] private Text coinText;
+    [SerializeField] private Text paperText;
+    [SerializeField] private AudioSource collectionSoundEffect;
+     [SerializeField] private GameObject portal;
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if(collision.gameObject.CompareTag("Coin"))
+        {
+            collectionSoundEffect.Play();
+            Destroy(collision.gameObject);
+            _coins++;
+        }
+
+        else if (collision.gameObject.CompareTag("Pages"))
+        {
+            Destroy(collision.gameObject);
+            _paper++;
+        }
+        if(_coins >= 20 && _paper==2 ){
+            Instantiate(portal,new Vector3(1152,7,0),Quaternion.identity);
+        }
+        coinText.text="Coins:" + _coins;
+        paperText.text="Paper"+_paper;
+
+    }
+}
