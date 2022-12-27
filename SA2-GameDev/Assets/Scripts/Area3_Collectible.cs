5bdbe2c6dd692e9c87a46b32d7b9ee8749d3cf1d
duwpo <duwpolikesrice@gmail.com>
2022-12-28 00:06:36 +0800

    final update for now

--- SA2-GameDev/Assets/Scripts/Area3_Collectible.cs
+++ SA2-GameDev/Assets/Scripts/Area3_Collectible.cs
@@ -11,11 +11,23 @@ public class Area3_Collectible : MonoBehaviour
     [SerializeField] private AudioSource collectionSoundEffect;
     [SerializeField] private GameObject endFlag;
     [SerializeField] private GameObject endPanel;
+    [SerializeField] private GameObject returnButton;
+    [SerializeField] private Button talkButton;
+    private bool _isPanelOpen;
+    private bool _isPlayerClose;
+    private bool isMobile;
 
     private void Start()
     {
+        #if UNITY_STANDALONE_WIN
+        isMobile = false;
+#elif UNITY_ANDROID || UNITY_IOS
+        isMobile = true;
+        #endif
         endFlag.SetActive(false);
         endPanel.SetActive(false);
+        _isPanelOpen = false;
+        _isPlayerClose = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -32,18 +44,77 @@ public class Area3_Collectible : MonoBehaviour
             Destroy(collision.gameObject);
             _paper++;
         }
-        if(_coins >= 30 && _paper==4 )
-        {
-            endFlag.SetActive(true);   
-        }
+        // if(_coins >= 30 && _paper==4 )
+        // {
+        //     endFlag.SetActive(true);   
+        // }
 
         if (collision.gameObject.CompareTag("flag"))
         {
-            endPanel.SetActive(true);   
+            _isPlayerClose = true;
         }
        
         coinText.text="Coins:" + _coins;
         paperText.text="Paper:"+_paper;
 
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("flag"))
+        {
+            _isPlayerClose = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(_coins >= 30 && _paper==4)
+        {
+            endFlag.SetActive(true);
+            if (_isPlayerClose && Input.GetKeyDown(KeyCode.E) && !isMobile && !_isPanelOpen)
+            {
+                _isPanelOpen = true;
+                endPanel.SetActive(true);
+            }
+
+            if (_isPanelOpen && !isMobile && Input.GetKeyDown(KeyCode.Escape))
+            {
+                Button();
+            }
+
+            if (_isPlayerClose && isMobile && !_isPanelOpen)
+            {
+                talkButton.gameObject.SetActive(true);
+                talkButton.onClick.AddListener(InteractButton);
+            }
+
+            if (_isPanelOpen && isMobile)
+            {
+                returnButton.SetActive(true);
+            }
+
+            if (_isPanelOpen)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+
+
+        }
+    }
+    public void  Button()
+    {
+        endPanel.SetActive(false);
+        _isPanelOpen = false;
+    }
+
+    public void InteractButton()
+    {
+        _isPanelOpen = true;
+        endPanel.SetActive(true);
     }
 }
