4a5bd43e5498d08b92d689e84de111d3bb6dd9f1
duwpo <duwpolikesrice@gmail.com>
2023-01-09 23:12:07 +0800

    added exit button at the end credit

--- SA2-GameDev/Assets/EndCredit.cs
+++ SA2-GameDev/Assets/EndCredit.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndCredit : MonoBehaviour
-{
-    // Start is called before the first frame update
+{ 
     void Start()
     {
         Time.timeScale = 1f;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            MainMenu();
+        }
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
