b8968828a9da123544608ecff280824a5bf8f419
duwpo <duwpolikesrice@gmail.com>
2023-02-01 12:47:25 +0800

    save load reset system improvements

--- SA2-GameDev/Assets/Scripts/PlayerSave.cs
+++ SA2-GameDev/Assets/Scripts/PlayerSave.cs
@@ -1,21 +1,52 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class PlayerSave : MonoBehaviour
 {
   public float x, y, z;
+  [SerializeField] private GameObject startButton;
+  [SerializeField] private GameObject loadButton;
+  [SerializeField] private GameObject resetButton;
 
   private void Start()
   {
     if(PlayerPrefs.GetInt("Saved") == 1)
     {
+      //startButton.SetActive(false);
+      //loadButton.SetActive(true);
+      //resetButton.SetActive(true);
       x = PlayerPrefs.GetFloat("x");
       y = PlayerPrefs.GetFloat("y");
       z = PlayerPrefs.GetFloat("z");
       transform.position = new Vector3(x, y, z);
       PlayerPrefs.SetInt("TimeToLoad",0);
       PlayerPrefs.Save();
+    }
+    else
+    {
+      //startButton.SetActive(true);
+      //loadButton.SetActive(false);
+      //resetButton.SetActive(false);
+    }
+    Scene currentScene = SceneManager.GetActiveScene();
+    string sceneName = currentScene.name;
+    
+    if(sceneName == "MainMenu") 
+    {
+      if (PlayerPrefs.GetInt("Saved") == 1)
+      {
+        startButton.SetActive(false);
+        loadButton.SetActive(true);
+        resetButton.SetActive(true);
+      }
+      else
+      {
+        startButton.SetActive(true);
+        loadButton.SetActive(false);
+        resetButton.SetActive(false);
+      }
     }
   }
 
@@ -35,15 +66,13 @@ public class PlayerSave : MonoBehaviour
     
     PlayerPrefs.Save();
     SceneManager.LoadScene("SampleScene");
+    
+  }
 
-    //x = PlayerPrefs.GetFloat("x");
-    // y = PlayerPrefs.GetFloat("y");
-    //z = PlayerPrefs.GetFloat("z");
-
-    //Vector3 loadPosition = new Vector3(x, y, z);
-    //transform.position = loadPosition;
-
-
-
+  public void ResetData()
+  {
+    PlayerPrefs.DeleteAll();
+    loadButton.SetActive(false);
+    startButton.SetActive(true);
   }
 }
