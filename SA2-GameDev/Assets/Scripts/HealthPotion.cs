4b4ebbfe25afe7f095dc28603ca18e0bc9e71699
duwpo <duwpolikesrice@gmail.com>
2023-02-01 19:54:55 +0800

    potions can only be picked up once

--- SA2-GameDev/Assets/Scripts/HealthPotion.cs
+++ SA2-GameDev/Assets/Scripts/HealthPotion.cs
@@ -1,12 +1,36 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class HealthPotion : MonoBehaviour
-{ 
+{
+    private bool _hasAlreadyUsed;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("SavePotion") == 1)
+        {
+            _hasAlreadyUsed = (PlayerPrefs.GetInt("Potion") != 0);
+            Debug.Log("You already picked up this potion");
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("Player") && HealthCount.Health <= 4)
+        if (col.gameObject.CompareTag("Player") && HealthCount.Health <= 4 && !_hasAlreadyUsed)
+        {
             HealthCount.Health += 1;
-        Debug.Log("you picked up a HP");
+            Debug.Log("you picked up a HP");
+            _hasAlreadyUsed = true;
+        }
+            
+        
+        
+    }
+
+    public void SavePotionState()
+    {
+        PlayerPrefs.SetInt("SavePotion", 1);
+        PlayerPrefs.SetInt("Potion", _hasAlreadyUsed ? 1 : 0);
     }
 }
