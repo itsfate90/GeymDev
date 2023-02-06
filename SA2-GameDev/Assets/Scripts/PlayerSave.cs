1e48f0c7cf0745f0ebff93e9eaf5c87109852eb6
duwpo <duwpolikesrice@gmail.com>
2023-02-06 17:42:44 +0800

    fix

--- SA2-GameDev/Assets/Scripts/PlayerSave.cs
+++ SA2-GameDev/Assets/Scripts/PlayerSave.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Numerics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
@@ -8,25 +9,53 @@ using Vector3 = UnityEngine.Vector3;
 public class PlayerSave : MonoBehaviour
 {
   
-
   public float x, y, z;
   [SerializeField] private GameObject startButton;
   [SerializeField] private GameObject loadButton;
   [SerializeField] private GameObject resetButton;
+  
 
   private void Start()
   {
-    if(PlayerPrefs.GetInt("Saved") == 1)
-    {
-      x = PlayerPrefs.GetFloat("x");
-      y = PlayerPrefs.GetFloat("y");
-      z = PlayerPrefs.GetFloat("z");
-      transform.position = new Vector3(x, y, z);
-      PlayerPrefs.Save();
-    }
+   
     Scene currentScene = SceneManager.GetActiveScene();
     string sceneName = currentScene.name;
-    
+    if(PlayerPrefs.GetInt("Saved1") == 1)
+    {
+      if (sceneName == "SampleScene")
+      {
+        x = PlayerPrefs.GetFloat("x1");
+        y = PlayerPrefs.GetFloat("y1");
+        z = PlayerPrefs.GetFloat("z1");
+        transform.position = new Vector3(x, y, z);
+        PlayerPrefs.Save();
+      }
+
+      if (PlayerPrefs.GetInt("Saved2") == 1)
+      {
+        if (sceneName == "Area2")
+        {
+          x = PlayerPrefs.GetFloat("x2");
+          y = PlayerPrefs.GetFloat("y2");
+          z = PlayerPrefs.GetFloat("z2");
+          transform.position = new Vector3(x, y, z);
+          PlayerPrefs.Save();
+        }
+      }
+      if (PlayerPrefs.GetInt("Saved3") == 1)
+      {
+        if (sceneName == "Area3")
+        {
+          x = PlayerPrefs.GetFloat("x2");
+          y = PlayerPrefs.GetFloat("y2");
+          z = PlayerPrefs.GetFloat("z2");
+          transform.position = new Vector3(x, y, z);
+          PlayerPrefs.Save();
+        }
+      }
+
+   
+    }
     
     if(sceneName == "MainMenu") 
     {
@@ -48,12 +77,36 @@ public class PlayerSave : MonoBehaviour
 
   public void position_save()
   {
+    PlayerPrefs.SetInt("Saved",1);
+    Scene currentScene = SceneManager.GetActiveScene();
+    string sceneName = currentScene.name;
+    
     Debug.Log("Saved!");
-   PlayerPrefs.SetFloat("x",transform.position.x);
-   PlayerPrefs.SetFloat("y", transform.position.y);
-   PlayerPrefs.SetFloat("z", transform.position.z);
+    if (sceneName == "SampleScene")
+    {
+      PlayerPrefs.SetInt("Saved1", 1);
+      PlayerPrefs.SetFloat("x1",transform.position.x);
+      PlayerPrefs.SetFloat("y1", transform.position.y);
+      PlayerPrefs.SetFloat("z1", transform.position.z);
+    }
+
+    else if (sceneName == "Area2")
+    {
+      PlayerPrefs.SetInt("Saved2", 1);
+      PlayerPrefs.SetFloat("x2",transform.position.x);
+      PlayerPrefs.SetFloat("y2", transform.position.y);
+      PlayerPrefs.SetFloat("z2", transform.position.z);
+    }
+    else if(sceneName == "Area3")
+    {
+      PlayerPrefs.SetInt("Saved3", 1);
+      PlayerPrefs.SetFloat("x3",transform.position.x);
+      PlayerPrefs.SetFloat("y3", transform.position.y);
+      PlayerPrefs.SetFloat("z3", transform.position.z);
+    }
+    
    
-   PlayerPrefs.SetInt("Saved", 1);
+   
    PlayerPrefs.SetInt("savedScene", SceneManager.GetActiveScene().buildIndex);
    PlayerPrefs.Save();
     
